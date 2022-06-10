5167032e7488d614cd2415b611e84bc2df7010e1
nikhilmotivitylabs <106816016+nikhilmotivitylabs@users.noreply.github.com>
2022-06-10 16:30:44 +0530

    assignment3

---
+++ assignment3/src/com/calculator/Calculator.java
@@ -0,0 +1,12 @@
+package com.calculator;
+
+public class Calculator {
+	
+	static public int powerInt(int num1,int num2) {
+		 return (int)Math.pow(num1, num2);
+	}
+	static public int powerDouble(double num1,int num2) {
+		return (int)Math.pow(num2, num1);
+	}
+
+}
---
+++ assignment3/src/com/calculator/Test.java
@@ -0,0 +1,15 @@
+package com.calculator;
+
+import com.calculator.*;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(Calculator.powerInt(10, 2));
+		
+		System.out.println(Calculator.powerDouble(5.5f, 5));
+	}
+
+}
